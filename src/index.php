f39a92347b56053936146e27a2f4134a981f556d
AymanZayed <ayman.safa@safavisa.com>
2020-07-07 15:40:34 +0200

    add some functions

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@ namespace Base\HelloWorld;
 
 class Index
 {
+    private $message;
+
     /**
      * @param string $greet
      * @return string
@@ -11,5 +13,18 @@ class Index
     public function greet($greet = "Hello World")
     {
         return $greet;
+    }
+
+    public function set($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
     }
 }
